7a891ca44ee4bbbea959b0100fd59855a4b921e7
9000927 <9000927@partner.onestorecorp.com>
2021-04-09 10:40:34 +0900

    first commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+<!-- html -->
+  <div>
+    header
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoHeader"
+  // js script
+}
+</script>
+
+<style scoped>
+/* css */
+</style>
